ea1d1a6bbad9b04c064eeed0a6d42831e12d41ba
Robert Cook <robertc@tigereyeconsulting.com>
2020-11-04 16:01:09 +0000

    Hangman tweak.

--- hangman.py
+++ hangman.py
@@ -1,16 +1,32 @@
 # Hangman.
 
-MAX_INCORRECT_LETTERS = 5
+MAX_WRONG_GUESSES = 5
 
 
 def show_word(word, letters):
     print("Word: ", end="")
-    for c in word:
-        if c in letters:
-            print(c, end="")
+    for letter in word:
+        if letter in letters:
+            print(letter, end="")
         else:
             print("-", end="")
     print()
+
+
+def show_wrong_guesses(guesses):
+    print("Wrong guesses: ", end="")
+    for letter in guesses:
+        print(letter, end="")
+    print()
+
+
+def get_letter():
+    letter = ""
+    while letter == "":
+        letter = input("Guess a letter > ")
+        if letter != "":
+            letter = letter[0]
+    return letter
 
 
 word = "something" # TODO: read this from a list of common words (part 2).
@@ -19,20 +35,14 @@ incorrect_letters = []
 
 
 # Game loop.
-while len(guessed_letters) != len(word) and len(incorrect_letters) < MAX_INCORRECT_LETTERS:
+while len(guessed_letters) != len(word) and len(incorrect_letters) < MAX_WRONG_GUESSES:
     show_word(word, guessed_letters)
-    print("Wrong guesses: ", end="")
-    for l in incorrect_letters:
-        print(l, end="")
-    print()
-    picked_letter = ""
-    while picked_letter == "":
-        picked_letter = input("Guess a letter > ")
-        picked_letter = picked_letter[0]
-    if picked_letter in word:
-        guessed_letters.append(picked_letter)
-    else:
-        incorrect_letters.append(picked_letter)
+    show_wrong_guesses(incorrect_letters)
+    letter = get_letter()
+    if letter in word and letter not in guessed_letters:
+        guessed_letters.append(letter)
+    elif letter not in word and letter not in incorrect_letters:
+        incorrect_letters.append(letter)
 # End of game message.
 if len(guessed_letters) == len(word):
     show_word(word, guessed_letters)
