ff6c4f5115a63d533fc98da0aab86d1deaf66026
Robert Cook <robertc@tigereyeconsulting.com>
2020-11-05 15:37:19 +0000

    Added python snippets module.

---
+++ snippets.py
@@ -0,0 +1,13 @@
+# Snippets.
+
+# Generic template of a decorator function.
+import functools
+
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator
