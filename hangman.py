dfb4cc62c9d6067e3f162ceeafdcbec336d4e23a
Robert Cook <message@robcook.me.uk>
2020-11-05 19:35:08 +0000

    Updated hangman.

--- hangman.py
+++ hangman.py
@@ -1,8 +1,5 @@
 # Hangman.
 
-MAX_WRONG_GUESSES = 5
-
-
 def show_word(word, letters):
     print("Word: ", end="")
     for letter in word:
@@ -29,24 +26,26 @@ def get_letter():
     return letter
 
 
-word = "something" # TODO: read this from a list of common words (part 2).
+word = "cat" # TODO: read this from a list of common words (part 2).
 guessed_letters = []
 incorrect_letters = []
 
 
 # Game loop.
+MAX_WRONG_GUESSES = 5
+
 while len(guessed_letters) != len(word) and len(incorrect_letters) < MAX_WRONG_GUESSES:
     show_word(word, guessed_letters)
     show_wrong_guesses(incorrect_letters)
     letter = get_letter()
     if letter in word and letter not in guessed_letters:
         guessed_letters.append(letter)
-    elif letter not in word and letter not in incorrect_letters:
+    elif letter not in incorrect_letters:
         incorrect_letters.append(letter)
+
 # End of game message.
 if len(guessed_letters) == len(word):
     show_word(word, guessed_letters)
-    print("Well done! You guessed the word with {} wrong guesses.".format(
-        len(incorrect_letters)))
+    print(f"Well done! You guessed the word with {len(incorrect_letters)} wrong guesses.")
 else:
     print(f"Too many wrong guesses! The word was '{word}'")
